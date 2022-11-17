9ecf9529c68fc381e49f854bea9bbc42c5950d7b
Simon Wörpel <simon.woerpel@medienrevolte.de>
2022-11-17 13:53:30 +0100

    Fix scheme guessing

--- farmsubsidy_store/schemes.py
+++ farmsubsidy_store/schemes.py
@@ -1,4 +1,5 @@
 import os
+import re
 from functools import lru_cache
 
 import pandas as pd
@@ -9,7 +10,23 @@ schemes = schemes.set_index("scheme_id")
 schemes["scheme_id_lower"] = schemes.index.str.lower()
 schemes["scheme_article_lower"] = schemes["scheme_article"].str.lower()
 schemes["scheme_name_lower"] = schemes["scheme_name"].str.lower()
-schemes["scheme_full_name"] = schemes.index + " - " + schemes["scheme_name"]
+schemes["scheme_full_name"] = (
+    schemes.index + " - " + schemes["scheme_name"].fillna("")
+).str.rstrip(" - ")
+
+
+re_ids = re.compile(
+    "|".join(
+        [
+            "(" + i.replace("/", "\/").replace(".", "\.") + "\s)"  # noqa
+            for i in sorted(schemes.index, key=lambda x: len(x), reverse=True)
+        ],
+    ),
+    re.IGNORECASE,
+)
+# MANUAL = {
+#     "single payment": schemes.loc[""]
+# }
 
 
 @lru_cache(None)
@@ -17,12 +34,12 @@ def guess_scheme(value: str):
     if not generate(value):
         return
     value = str(value)
-    value = value.lower()
+    m = re_ids.search(value)
+    if m is not None:
+        return schemes.loc[m.group().strip()]["scheme_full_name"]
     for _, row in schemes.iterrows():
-        if row["scheme_id_lower"] in value:
-            return row["scheme_full_name"]
         if not pd.isna(row["scheme_name_lower"]):
-            if value in row["scheme_name_lower"]:
+            if value.lower() in row["scheme_name_lower"]:
                 return row["scheme_full_name"]
     return value
 
